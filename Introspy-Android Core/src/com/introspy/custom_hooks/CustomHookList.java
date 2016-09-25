be7b79da48fc12d0d86f15d620f4c9d8615fee0e
jay-tang <1052711046@qq.com>
2016-09-25 20:07:19 +0800

    加了个cydia写的脱壳机

--- Introspy-Android Core/src/com/introspy/custom_hooks/CustomHookList.java
+++ Introspy-Android Core/src/com/introspy/custom_hooks/CustomHookList.java
@@ -11,7 +11,7 @@ public class CustomHookList {
 	static private HookConfig[] _hookList = new HookConfig[] {
 
 	new HookConfig(true, // set to true to enable the hook
-			"java.lang.String", "equals", new Class<?>[] { Object.class },
+			"java.io.File", "delete", new Class<?>[] {},
 			// class, method name, arguments
 			new HookExampleImpl(),
 			// instance of the implementation extending IntroHook (here in
