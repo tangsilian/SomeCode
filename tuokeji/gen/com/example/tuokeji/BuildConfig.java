ae0cad6f3d566d839a775493b1e79da101b4cb71
endlif# <1052711046@qq.com>
2016-12-11 19:55:10 +0800

    delete some eclipse demo
    delete some eclipse demo

--- tuokeji/gen/com/example/tuokeji/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.tuokeji;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- tuokeji/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := DumpDex.cy
-LOCAL_SRC_FILES := DumpDex.cy.cpp
-
-include $(BUILD_SHARED_LIBRARY)
